68957a1a68d3ac5b7e3801ea471b8751079c49a2
frankcs96 <fcsuarez96@gmail.com>
2020-05-24 18:31:18 +0200

    Updates

---
+++ precache-manifest.2a535a9f4a49a179b4ad552da5cadba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde72be6a82d7c020ede6ee3ebc5d0b0",
+    "url": "/www.fcscode.tech/index.html"
+  },
+  {
+    "revision": "0eb948c3c64d3e0267e8",
+    "url": "/www.fcscode.tech/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "46a326c8cf2cd8e8be1d",
+    "url": "/www.fcscode.tech/static/js/2.f539d5a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/www.fcscode.tech/static/js/2.f539d5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb948c3c64d3e0267e8",
+    "url": "/www.fcscode.tech/static/js/main.41a0a36d.chunk.js"
+  },
+  {
+    "revision": "5eadd68fe4a7d487636b",
+    "url": "/www.fcscode.tech/static/js/runtime-main.bd05fa9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.f6fd572a1e6ea0d6c0ebb7f01e141236.js"
+  "/www.fcscode.tech/precache-manifest.2a535a9f4a49a179b4ad552da5cadba0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.fcscode.tech/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
